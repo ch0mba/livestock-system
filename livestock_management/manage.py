90d72652f759398260fb8e3b04a08739164ecb30
ch0mba <chombaerickdickson@gmail.com>
2025-05-16 17:22:56 +0300

    manage.py change settings

--- livestock_management/manage.py
+++ livestock_management/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'livestock_management.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'livestock_management.livestock_management.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
